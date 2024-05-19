2e38621630dcfc486b39eecfa58e322c157cd79d
karthikthiru11 <karthikthirupathy11@gmail.com>
2024-05-19 12:14:41 +0530

    Change Name

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 const heading= React.createElement("h1",{
   id:"heading1"
-},"Hello bkjhsj");
+},"Hello User");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
